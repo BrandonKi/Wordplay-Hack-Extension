0cdd9e2db00f23432d9eea150c97a3543af81eeb
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-01-03 14:17:51 -0500

    Update content.js

--- content.js
+++ content.js
@@ -11,12 +11,42 @@ chrome.runtime.onMessage.addListener(
         chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
       }
       else if (request.message === "done-loading"){
-          const username = document.getElementById('username');
-          username.value = request.username;
-          username.dispatchEvent(new Event('change'));
-          const password = document.getElementById('password');
-          password.value = request.password;
-          password.dispatchEvent(new Event('change'));
+        const username = document.getElementById('username');
+        const password = document.getElementById('password');
+        const login = getElementByXpath("/html/body/div/div/div/div/div/div/div/div[1]/form/button");
+        // This updates the value on the DOM, but not the displayed value:
+        // username.setAttribute('value', 'Bob');
+
+        // This updates the value displayed on the input, but not the DOM:
+        //username.value = 'Lumos';
+
+        // Dispatch an "input" event. In some cases, "change" would also work:
+        //username.dispatchEvent(new Event('input', { bubbles: true }));
+
+        // It looks like only one field will be updated if both events are dispatched
+        // straight away, so you could use a setTimeout here:
+        username.focus();
+        document.execCommand('insertText', false, 'Lumos');
+        password.focus();
+        document.execCommand('insertText', false, 'Wordplay');
+        login.click();
+        
       }
     }
   );
+
+  function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  }
+  
+  function waitForElementToDisplay(selector, time) {
+    if(document.querySelector(selector)!=null) {
+        alert("The element is displayed, you can put your code instead of this alert.")
+        return;
+    }
+    else {
+        setTimeout(function() {
+            waitForElementToDisplay(selector, time);
+        }, time);
+    }
+}
