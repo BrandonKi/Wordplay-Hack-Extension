397c8d1ea9f6518b79a08e6e257866c07a1d569b
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-01-03 19:56:41 -0500

    Update background.js

--- background.js
+++ background.js
@@ -3,6 +3,7 @@
 // Called when the user clicks on the browser action.
 var username;
 var password;
+var activeTab;
 // chrome.browserAction.onClicked.addListener(function(tab) {
 //   });
   
@@ -21,21 +22,19 @@ var password;
                 username = result.name;
                 chrome.storage.sync.get(['pass'], function(result) {
                   password = result.pass;
-                  var des = confirm("Username: " + username + "\nPassword: " + password);
-                  if (des){
-                      txt = "Confirmed";
-                  } else {
-                      txt = "User cancelled the prompt.";
-                  }
-                  alert(txt);
-                  var activeTab = tabs[0];
-                  chrome.tabs.sendMessage(activeTab.id, {"message": "done-loading", username: username, password: password});
+                  // var des = confirm("Please Confirm Username and PasswordUsername: " + username + "\nPassword: " + password);
+                  // if (des){
+                    
+                  // } else {
+                  //     throw new Error("Incorrect Username and Password");
+                  // }
                 });
               });
-    
-              
-            
+              activeTab = tabs[0];
         });
+        setTimeout( function() {
+          chrome.tabs.sendMessage(activeTab.id, {"message": "done-loading", username: username, password: password});
+        }, 1000);
       }
     }
   );
